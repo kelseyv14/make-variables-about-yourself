aa19487b8f4ebda8dcacc3dbdf1db23f6542b0a7
kelsey vaught <kelsey.vaught92@gmail.com>
2017-02-13 13:25:26 -0500

    made about me js

---
+++ js/global.js
@@ -0,0 +1,12 @@
+
+
+var fullName = 'kelsey vaught';
+var action1 = 'tip toed';
+var noun1 = 'persian cat';
+var story = 'One day, ' + fullName + ' ' + action1 + ' over the ' + noun1;
+
+// var story = `One day, ${fullName} ${action1} over the ${noun1}.`;
+
+console.log(story);
+
+
