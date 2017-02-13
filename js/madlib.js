a7c2dcc8e59320abd8929fc70fa76a75e4827883
kelsey vaught <kelsey.vaught92@gmail.com>
2017-02-13 15:31:04 -0500

    filled in story

--- js/madlib.js
+++ js/madlib.js
@@ -1,30 +1,30 @@
-var pNoun = '';
-var pNoun2 = '';
-var adj = '';
-var pNoun3 = '';
-var pNounStartR = ''; 
-var nounStartS = '';
-var verbIng = '';
-var adj2 = '';
-var sillyName = '';
-var animal = '';
-var verbIng2 = '';
-var adj3 = '';
-var spanishName = '';
-var noun = '';
-var number = '';
-var verbEd = '';
-var adj4 = '';
-var occupation = '';
-var noun2 = '';
-var govPosition = '';
-var verbEd2 = '';
-var noun3 = '';
-var occupation2 = '';
-var noun4 = '';
+var pNoun = 'cups';
+var pNoun2 = 'french fries,';
+var adj = 'soft pink';
+var pNoun3 = 'tap shoes';
+var pNounStartR = 'retnias'; 
+var nounStartS = 'spoon';
+var verbIng = 'spinning';
+var adj2 = 'powder bleu';
+var sillyName = 'doodle_queen420';
+var animal = 'kitty';
+var verbIng2 = 'flipping';
+var adj3 = 'twinkley';
+var spanishName = 'fransisco';
+var noun = 'tea cup';
+var number = '444';
+var verbEd = 'chirped';
+var adj4 = 'silky';
+var occupation = 'CEO';
+var noun2 = 'crystal ball';
+var govPosition = 'Supreme Court Justice';
+var verbEd2 = 'winked';
+var verbEd3 = 'tickled';
+var occupation2 = 'pastry chef';
+var noun4 = 'mouseketeer';
 
 
-var story = [' Return to a time when men were ' + pNoun + ' and swamps were ' + pNoun2 + ' ' + adj + ' ' + ' Swamps, that is. Full of lightning- ' + pNoun3 + ' and ' + pNounStartR + ' of Unusual ' + nounStartS + ' Lagoons were inhabited by ' + verbIng + ' eels. And the most ' + adj2 + ' woman in the world was named ' + sillyName + ' Well, it`s a bent ' + animal + ' tale. Complete with all the fencing, ' + verbIng2 + ' , escapes, and ' + adj3 + ' accents you`d expect. Including such unique folk as ' + spanishName + ' , who has dreamt his whole ' + noun + ' of finding the ' + number + ' fingered man who ' + verbEd + 'his father, and his' + adj4 + 'sidekick, Fezzig. Blonde' + occupation + 'loves Westley, a poor' + noun2 + 'But when he`s captured by' + govPosition + 's, she`s chosen by evil' + verbEd2 + 'Humperdinck to be his princess bride. Along the way, she gets' + noun3 + 'by the Dread' + occupation2 + 'Roberts, the spaniard gets his wish, and Humperdinck turns out to be a' + noun4] 
+var story = ' Return to a time when men were ' + pNoun + ' and swamps were ' + pNoun2 + ' ' + adj + ' ' + ' Swamps, that is. Full of lightning- ' + pNoun3 + ' and ' + pNounStartR + ' of Unusual ' + nounStartS + ' Lagoons were inhabited by ' + verbIng + ' eels. And the most ' + adj2 + ' woman in the world was named ' + sillyName + ' Well, it`s a bent ' + animal + ' tale. Complete with all the fencing, ' + verbIng2 + ' , escapes, and ' + adj3 + ' accents you`d expect. Including such unique folk as ' + spanishName + ' , who has dreamt his whole ' + noun + ' of finding the ' + number + ' fingered man who ' + verbEd + ' his father, and his ' + adj4 + ' sidekick, Fezzig. Blonde ' + occupation + ' loves Westley, a poor ' + noun2 + ' But when he`s captured by ' + govPosition + 's, she`s chosen by evil ' + verbEd2 + ' Humperdinck to be his princess bride. Along the way, she gets' + verbEd3 + ' by the Dread ' + occupation2 + ' Roberts, the spaniard gets his wish, and Humperdinck turns out to be a ' + noun4;
 
 console.log (story);
 
