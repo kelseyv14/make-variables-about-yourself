2fd79ddf1842bc9c4628077a2f66f9749332683a
kelsey vaught <kelsey.vaught92@gmail.com>
2017-02-13 14:01:13 -0500

    test

---
+++ js/about-me.js
@@ -0,0 +1,27 @@
+var firstName = 'kelsey';
+var lastName = 'vaught';
+var fullName = firstName + ' ' + lastName;
+var emailAddress = 'kelsey.vaught92@gmail.com';
+var noblesville = true;
+var collegeYears = 2;
+var pets = 1;
+var sleepHours = parceFloat('8')
+var music = 'disco';
+var moviesShows = [`Forgien-French`, `Game Shows`];
+var jobTitle 
+var withoutJava = null;
+
+console.log (firstName);
+console.log (lastName);
+console.log (fullName);
+console.log (emailAddress);
+console.log (noblesville);
+console.log (collegeYears);
+console.log (pets);
+console.log (sleepHours);
+console.log (music);
+console.log (moviesShows);
+console.log (jobTitle);
+console.log (withoutJava);
+
+
